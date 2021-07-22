a334f86a1e39a113e4dc254f9522b7db937fd71e
GEONYEONG SON <thsrjs5@gmail.com>
2021-07-22 18:11:21 +0900

    create module server upload video

---
+++ boilerplate-mern-stack-master/server/routes/video.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const router = express.Router();
+const { Video } = require('../models/Video');
+
+const { auth } = require('../middleware/auth');
+const multer = require('multer');
+const ffmpeg = require('fluent-ffmpeg');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.mp4') {
+      return cb(res.status(400).end('only mp4 is allowed'), false);
+    } else {
+      cb(null, true);
+    }
+  },
+});
+
+const upload = multer({ storage: storage }).single('file');
+
+//=================================
+//             Video
+//=================================
+
+router.post('/uploadfiles', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      url: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+router.post('/uploadVideo', (req, res) => {
+  const video = new Video(req.body);
+
+  video.save((err, doc) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    res.status(200).json({ success: true });
+  });
+});
+
+router.post('/thumbnail', (req, res) => {
+  let filePath = '';
+  let fileDuration = '';
+
+  //비디오 정보 가져오기
+  ffmpeg.ffprobe(req.body.url, function (err, metadata) {
+    console.dir(metadata);
+    console.log(metadata.format.duration);
+    fileDuration = metadata.format.duration;
+  });
+
+  //썸네일 생성
+  ffmpeg(req.body.url)
+    .on('filenames', function (filenames) {
+      console.log('Will generate ' + filenames.join(', '));
+      console.log(filenames);
+
+      filePath = 'uploads/thumbnails/' + filenames[0];
+    })
+    .on('end', function () {
+      console.log('Screenshots taken');
+      return res
+        .json({
+          success: true,
+          url: filePath,
+          fileDuration: fileDuration,
+        })
+        .on('error', function (err) {
+          console.error(err);
+          return res.json({ success: false, err });
+        })
+        .screenshots({
+          count: 3,
+          folder: `uploads/thumbnails`,
+          size: `320*240`,
+          filename: 'thumbnail-%b.png',
+        });
+    });
+});
+
+module.exports = router;
